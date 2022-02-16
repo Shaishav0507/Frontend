46bcc76eb652d483961b218679f9fb069cd5f18d
Shivaansh17 <shivansh_rastogi@hotmail.com>
2022-02-16 10:59:11 +0530

    deleted files

--- src/app/containers/Login.js
+++ src/app/containers/Login.js
@@ -74,7 +74,7 @@ const Login = ({ login, isAuthenticated }) => {
                         >
                             Login
                         </button>
-                        {/* <button
+                        <button
                             className="btn btn-primary btn-block"
                             style={{ borderRadius: '50px 50px', width: '180px', marginLeft: '15px' }}
                             type="submit"
@@ -82,7 +82,7 @@ const Login = ({ login, isAuthenticated }) => {
                             <Link to="/launchpad" style={{ color: 'white' }}>
                                 LaunchPad
                             </Link>
-                        </button> */}
+                        </button>
                     </form>
                 </div>
                 <p className="mt-3">
