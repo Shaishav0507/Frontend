cd1f61967cdd248640423c1997155e418caed56a
ArshWalker <arshwalker7@gmail.com>
2022-02-20 02:21:27 +0530

    Forms changed to react

--- src/app/containers/StartPage/Usersform.js
+++ src/app/containers/StartPage/Usersform.js
@@ -10,6 +10,7 @@ import {
 import { styled } from '@mui/system'
 import { Span } from 'app/components/Typography'
 import React, { Component } from 'react'
+import { Form } from 'react-bootstrap'
 import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator'
 
 const TextField = styled(TextValidator)(() => ({
@@ -58,6 +59,7 @@ export class Usersform extends Component {
                 <ValidatorForm
                     onSubmit={this.handleSubmit}
                     onError={() => null}
+                    {...this.props}
                 >
                     <Grid container spacing={6}>
                         <Grid item lg={6} md={4} sm={12} xs={12} sx={{ mt: 2 }}>
@@ -71,30 +73,65 @@ export class Usersform extends Component {
                              validators={['required']}
                              errorMessages={['this field is required']}
                          /> */}
-
-                            <TextField
-                                label="First Name"
-                                type="text"
-                                name="Name"
-                                //   value={fname}
-                                validators={['required']}
-                                errorMessages={['this field is required']}
-                            />
-                            <TextField
-                                label="Last Name"
-                                type="text"
-                                name="gst"
-                                //   value={lname || ''}
-                            />
-                            <TextField
-                                label="Bussiness"
-                                type="text"
-                                name="pan"
-                                //   value={pan || ''}
-                                validators={['required']}
-                                errorMessages={['this field is required']}
-                            />
-
+                            <Form.Group controlId="fname">
+                                <Form.Control
+                                    //  className=" mb-2 p-2"
+                                    style={{
+                                        width: '100%',
+                                        marginBottom: '16px',
+                                    }}
+                                    size="lg"
+                                    type="text"
+                                    placeholder="First Name"
+                                    name="Name"
+                                />
+                                {/* <TextField
+                                    label="First Name"
+                                    type="text"
+                                    name="Name"
+                                    //   value={fname}
+                                    validators={['required']}
+                                    errorMessages={['this field is required']}
+                                /> */}
+                            </Form.Group>
+                            <Form.Group controlId="lname">
+                                <Form.Control
+                                    style={{
+                                        width: '100%',
+                                        marginBottom: '16px',
+                                    }}
+                                    size="lg"
+                                    type="text"
+                                    placeholder="Last Name"
+                                    name="Name"
+                                />
+                                {/* <TextField
+                                    label="Last Name"
+                                    type="text"
+                                    name="gst"
+                                    //   value={lname || ''}
+                                /> */}
+                            </Form.Group>
+                            <Form.Group controlId="business">
+                                <Form.Control
+                                    style={{
+                                        width: '100%',
+                                        marginBottom: '16px',
+                                    }}
+                                    size="lg"
+                                    type="text"
+                                    placeholder="Bussiness"
+                                    name="Name"
+                                />
+                                {/* <TextField
+                                    label="Bussiness"
+                                    type="text"
+                                    name="pan"
+                                    //   value={pan || ''}
+                                    validators={['required']}
+                                    errorMessages={['this field is required']}
+                                /> */}
+                            </Form.Group>
                             <FormControlLabel
                                 control={<Checkbox />}
                                 label="I have read and agree to the terms of service."
@@ -102,15 +139,26 @@ export class Usersform extends Component {
                         </Grid>
 
                         <Grid item lg={6} md={6} sm={12} xs={12} sx={{ mt: 2 }}>
-                            <TextField
-                                label="Address"
-                                type="text"
-                                name="address"
-                                //   value={address || ''}
-                                validators={['required']}
-                                errorMessages={['this field is required']}
-                            />
-
+                            <Form.Group controlId="address">
+                                <Form.Control
+                                    style={{
+                                        width: '100%',
+                                        marginBottom: '16px',
+                                    }}
+                                    size="lg"
+                                    type="text"
+                                    placeholder="Address"
+                                    name="Name"
+                                />
+                                {/* <TextField
+                                    label="Address"
+                                    type="text"
+                                    name="address"
+                                    //   value={address || ''}
+                                    validators={['required']}
+                                    errorMessages={['this field is required']}
+                                /> */}
+                            </Form.Group>
                             {/* <TextField
                               label="State"
                               type="email"
@@ -122,77 +170,99 @@ export class Usersform extends Component {
                                   'email is not valid',
                               ]}
                             /> */}
-                            <div className="form-group d-block d-flex flex-column">
-                                <select
-                                    class="form-select form-select-lg mb-2 p-2"
-                                    aria-label="form-select-lg example"
+                            <Form.Group controlId="state">
+                                <div className="  d-block d-flex flex-column">
+                                    <select
+                                        class="form-select form-select-lg p-2"
+                                        aria-label="form-select-lg example"
+                                        style={{
+                                            width: '100%',
+                                            marginBottom: '16px',
+                                            color: 'gray',
+                                        }}
+                                        name="state"
+                                    >
+                                        <option value="AN">
+                                            Andaman and Nicobar Islands
+                                        </option>
+                                        <option value="AP">
+                                            Andhra Pradesh
+                                        </option>
+                                        <option value="AR">
+                                            Arunachal Pradesh
+                                        </option>
+                                        <option value="AS">Assam</option>
+                                        <option value="BR">Bihar</option>
+                                        <option value="CH">Chandigarh</option>
+                                        <option value="CT">Chhattisgarh</option>
+                                        <option value="DN">
+                                            Dadra and Nagar Haveli
+                                        </option>
+                                        <option value="DD">
+                                            Daman and Diu
+                                        </option>
+                                        <option value="DL" selected>
+                                            Delhi
+                                        </option>
+                                        <option value="GA">Goa</option>
+                                        <option value="GJ">Gujarat</option>
+                                        <option value="HR">Haryana</option>
+                                        <option value="HP">
+                                            Himachal Pradesh
+                                        </option>
+                                        <option value="JK">
+                                            Jammu and Kashmir
+                                        </option>
+                                        <option value="JH">Jharkhand</option>
+                                        <option value="KA">Karnataka</option>
+                                        <option value="KL">Kerala</option>
+                                        <option value="LA">Ladakh</option>
+                                        <option value="LD">Lakshadweep</option>
+                                        <option value="MP">
+                                            Madhya Pradesh
+                                        </option>
+                                        <option value="MH">Maharashtra</option>
+                                        <option value="MN">Manipur</option>
+                                        <option value="ML">Meghalaya</option>
+                                        <option value="MZ">Mizoram</option>
+                                        <option value="NL">Nagaland</option>
+                                        <option value="OR">Odisha</option>
+                                        <option value="PY">Puducherry</option>
+                                        <option value="PB">Punjab</option>
+                                        <option value="RJ">Rajasthan</option>
+                                        <option value="SK">Sikkim</option>
+                                        <option value="TN">Tamil Nadu</option>
+                                        <option value="TG">Telangana</option>
+                                        <option value="TR">Tripura</option>
+                                        <option value="UP">
+                                            Uttar Pradesh
+                                        </option>
+                                        <option value="UT">Uttarakhand</option>
+                                        <option value="WB">West Bengal</option>
+                                    </select>
+                                </div>
+                            </Form.Group>
+                            <Form.Group controlId="code">
+                                <Form.Control
                                     style={{
-                                        // width: '400px',
-                                        height: '60px',
-                                        fontFamily: 'sans',
-                                        color: 'gray',
-                                        fontSize: '18px',
+                                        width: '100%',
+                                        marginBottom: '16px',
                                     }}
-                                    name="state"
-                                >
-                                    <option value="AN">
-                                        Andaman and Nicobar Islands
-                                    </option>
-                                    <option value="AP">Andhra Pradesh</option>
-                                    <option value="AR">
-                                        Arunachal Pradesh
-                                    </option>
-                                    <option value="AS">Assam</option>
-                                    <option value="BR">Bihar</option>
-                                    <option value="CH">Chandigarh</option>
-                                    <option value="CT">Chhattisgarh</option>
-                                    <option value="DN">
-                                        Dadra and Nagar Haveli
-                                    </option>
-                                    <option value="DD">Daman and Diu</option>
-                                    <option value="DL" selected>
-                                        Delhi
-                                    </option>
-                                    <option value="GA">Goa</option>
-                                    <option value="GJ">Gujarat</option>
-                                    <option value="HR">Haryana</option>
-                                    <option value="HP">Himachal Pradesh</option>
-                                    <option value="JK">
-                                        Jammu and Kashmir
-                                    </option>
-                                    <option value="JH">Jharkhand</option>
-                                    <option value="KA">Karnataka</option>
-                                    <option value="KL">Kerala</option>
-                                    <option value="LA">Ladakh</option>
-                                    <option value="LD">Lakshadweep</option>
-                                    <option value="MP">Madhya Pradesh</option>
-                                    <option value="MH">Maharashtra</option>
-                                    <option value="MN">Manipur</option>
-                                    <option value="ML">Meghalaya</option>
-                                    <option value="MZ">Mizoram</option>
-                                    <option value="NL">Nagaland</option>
-                                    <option value="OR">Odisha</option>
-                                    <option value="PY">Puducherry</option>
-                                    <option value="PB">Punjab</option>
-                                    <option value="RJ">Rajasthan</option>
-                                    <option value="SK">Sikkim</option>
-                                    <option value="TN">Tamil Nadu</option>
-                                    <option value="TG">Telangana</option>
-                                    <option value="TR">Tripura</option>
-                                    <option value="UP">Uttar Pradesh</option>
-                                    <option value="UT">Uttarakhand</option>
-                                    <option value="WB">West Bengal</option>
-                                </select>
-                            </div>
-                            <TextField
-                                sx={{ mb: 2 }}
-                                label="State Code"
-                                type="text"
-                                name="monumber"
-                                //   value={monumber || ''}
+                                    size="lg"
+                                    type="text"
+                                    placeholder="State Code"
+                                    name="Name"
+                                />
+                                {/* <TextField
+                                    //  sx={{ mb: 2 }}
+                                    label="State Code"
+                                    type="text"
+                                    name="monumber"
+                                    //   value={monumber || ''}
 
-                                errorMessages={['this field is required']}
-                            />
+                                    errorMessages={['this field is required']}
+                                /> */}
+                            </Form.Group>
                         </Grid>
                     </Grid>
                     <Button
