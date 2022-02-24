6b1536bc807b29bccf1f759b4ddd3c6bcedac07e
Shivaansh17 <shivansh_rastogi@hotmail.com>
2022-02-24 16:05:38 +0530

    GST Form

--- src/app/containers/GSTForm.js
+++ src/app/containers/GSTForm.js
@@ -1,14 +1,9 @@
-import {
-    Button,
-    Icon,
-    Grid,
-    FormControlLabel,
-    Checkbox,
-} from '@mui/material'
+import { Button, Icon, Grid, FormControlLabel, Checkbox, Container } from '@mui/material'
+import SimpleCard from 'app/components/cards/SimpleCard'
 import { Span } from 'app/components/Typography'
 import React, { Component } from 'react'
 import { Form } from 'react-bootstrap'
-import { ValidatorForm} from 'react-material-ui-form-validator'
+import { ValidatorForm } from 'react-material-ui-form-validator'
 
 export class GSTForm extends Component {
     constructor(props) {
@@ -17,21 +12,23 @@ export class GSTForm extends Component {
     }
     handleSubmit(event) {
         event.preventDefault()
-        fetch('http://localhost:8000/customer/', {
+        fetch('http://localhost:8000/gst/', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                CustomerId: null,
-                Name: event.target.Name.value,
-                Address: event.target.Address.value,
-                GST: event.target.GST.value,
+                GSTId: null,
+                Fname: event.target.Fname.value,
+                Lname: event.target.Lname.value,
+                GSTNo: event.target.GSTNo.value,
                 Email: event.target.Email.value,
-                Pan: event.target.Pan.value,
-                Contact: event.target.Contact.value,
-                Bank:event.target.Bank.value,
+                Phone: event.target.Phone.value,
+                Address: event.target.Address.value,
+                State: event.target.State.value,
+                Code: event.target.Code.value,
+                Account: event.target.Account.value,
             }),
         })
             .then((res) => res.json())
@@ -49,128 +46,185 @@ export class GSTForm extends Component {
     render() {
         // const handleFileUpload = () => {}
         return (
-            <div>
-                <ValidatorForm
-                    onSubmit={this.handleSubmit}
-                    onError={() => null}
-                    {...this.props}
-                >
-                    <Grid container spacing={6}>
-                        <Grid item lg={6} md={4} sm={12} xs={12} sx={{ mt: 2 }}>
-                            <Form.Group controlId="Name">
-                                <Form.Control
-                                    //  className=" mb-2 p-2"
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Name"
-                                    name="Name"
-                                />
-                            </Form.Group>
-                            <Form.Group controlId="Address">
-                                <Form.Control
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Address"
-                                    name="Address"
-                                />
-                                
-                            </Form.Group>
-                            <Form.Group controlId="GST">
-                                <Form.Control
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="GST"
-                                    name="GST"
-                                />
-                                
-                            </Form.Group>
-                            <FormControlLabel
-                                control={<Checkbox />}
-                                label="I have read and agree to the terms of service."
-                            />
-                        </Grid>
-
-                        <Grid item lg={6} md={6} sm={12} xs={12} sx={{ mt: 2 }}>
-                            <Form.Group controlId="Email">
-                                <Form.Control
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Email"
-                                    name="Email"
-                                />
-                                
-                            </Form.Group>
-                            
-                            <Form.Group controlId="Pan">
-                                <Form.Control
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="PAN No."
-                                    name="Pan"
-                                />
-                                
-                            </Form.Group>
-                            <Form.Group controlId="Contact">
-                                <Form.Control
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Contact"
-                                    name="Contact"
-                                />
-                                
-                            </Form.Group>
-                            <Form.Group controlId="Bank">
-                                <Form.Control
-                                    style={{
-                                        width: '100%',
-                                        marginBottom: '16px',
-                                    }}
-                                    size="lg"
-                                    type="text"
-                                    placeholder="Account Number"
-                                    name="Banks"
-                                />
-                                
-                            </Form.Group>
-                        </Grid>
-                    </Grid>
-                    <Button
-                        color="primary"
-                        variant="contained"
-                        type="submit"
-                
+            <Container style={{ marginTop: '50px' }}>
+                <SimpleCard ><span style={{ fontSize: '20px', fontWeight: 'bold', marginLeft: '20px' }}>GST REGISTRATION</span>
+                    <ValidatorForm
+                        onSubmit={this.handleSubmit}
+                        onError={() => null}
+                        {...this.props}
                     >
-                        <Icon>add</Icon>
-                        <Span sx={{ pl: 1, textTransform: 'capitalize' }}>
-                            Add Customer
-                        </Span>
-                    </Button>
-                </ValidatorForm>
-            </div>
+                        <Grid container spacing={5}>
+                            <Grid
+                                item
+                                lg={6}
+                                md={10}
+                                sm={2}
+                                xs={20}
+                                sx={{ mt: 2 }}
+                            >
+                                <Form.Group controlId="Fname">
+                                    <Form.Control
+                                        //  className=" mb-2 p-2"
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            marginLeft: '50px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="First Name"
+                                        name="Fname"
+                                    />
+                                </Form.Group>
+
+                                <Form.Group controlId="GSTNo">
+                                    <Form.Control
+                                        //  className=" mb-2 p-2"
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            marginLeft: '50px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Gst No"
+                                        name="Gst no"
+                                    />
+                                </Form.Group>
+
+                                <Form.Group controlId="Phone">
+                                    <Form.Control
+                                        //  className=" mb-2 p-2"
+                                        style={{
+                                            width: '70%',
+                                            borderRadius: '15px',
+                                            marginLeft: '50px',
+                                            marginBottom: '16px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Phone Number"
+                                        name="Phone"
+                                    />
+                                </Form.Group>
+
+                                <Form.Group controlId="State">
+                                    <Form.Control
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            marginLeft: '50px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="State"
+                                        name="State"
+                                    />
+                                </Form.Group>
+
+                                <Form.Group controlId="Account">
+                                    <Form.Control
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            marginLeft: '50px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Account Number"
+                                        name="Account"
+                                    />
+                                </Form.Group>
+                                <FormControlLabel
+                                    style={{marginLeft: '50px',}}
+                                    control={<Checkbox />}
+                                    label="I have read and agree to the terms of service."
+                                />
+                            </Grid>
+
+                            <Grid
+                                item
+                                lg={6}
+                                md={6}
+                                sm={12}
+                                xs={12}
+                                sx={{ mt: 2 }}
+                            >
+                                <Form.Group controlId="Lname">
+                                    <Form.Control
+                                        //  className=" mb-2 p-2"
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Last Name"
+                                        name="Lname"
+                                    />
+                                </Form.Group>
+
+                                <Form.Group controlId="Email">
+                                    <Form.Control
+                                        //  className=" mb-2 p-2"
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Email"
+                                        name="email"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="Address">
+                                    <Form.Control
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Address"
+                                        name="Address"
+                                    />
+                                </Form.Group>
+                                <Form.Group controlId="Code">
+                                    <Form.Control
+                                        style={{
+                                            width: '70%',
+                                            marginBottom: '16px',
+                                            borderRadius: '15px'
+                                        }}
+                                        size="lg"
+                                        type="text"
+                                        placeholder="Pin Code"
+                                        name="Code"
+                                    />
+                                </Form.Group>
+                            </Grid>
+                        </Grid>
+                        <Button
+                            color="primary"
+                            style={{marginLeft: '50px', borderRadius: '15px'}}
+                            variant="contained"
+                            type="submit"
+                        >
+                            <Icon>add</Icon>
+                            <Span sx={{ pl: 1, textTransform: 'capitalize' }}>
+                                Proceed to Payments
+                            </Span>
+                        </Button>
+                    </ValidatorForm>
+                </SimpleCard>
+            </Container>
         )
     }
 }
